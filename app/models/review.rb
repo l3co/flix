25cdad1b0e36216ddbd10858b589e19c57aa5658
l3co <lcosta.sp.br@gmail.com>
2026-09-05 13:17:24 -0300

    feat : add reviews association to movies and validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
   belongs_to :movie
+
+  validates :name, presence: true
+  validates :comment, length: { minimum: 4 }
+
+  validates :stars, inclusion: {
+    in: 1..5,
+    message: "must be between 1 and 5"
+  }
 end
