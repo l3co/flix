950766b5c75ff37d1a816318cf5b90389e427248
l3co <lcosta.sp.br@gmail.com>
2026-09-08 14:07:56 -0300

    feat : implement review creation and display with improved layout

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,8 +4,10 @@ class Review < ApplicationRecord
   validates :name, presence: true
   validates :comment, length: { minimum: 4 }
 
+  STARS = [1, 2, 3, 4, 5]
+
   validates :stars, inclusion: {
-    in: 1..5,
+    in: STARS,
     message: "must be between 1 and 5"
   }
 end
