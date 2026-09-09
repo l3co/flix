1c0c173611d639dcb1aa8d1911d5638d9f8c7d2f
l3co <lcosta.sp.br@gmail.com>
2026-09-09 16:43:40 -0300

    feat : add user profile editing and deletion functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,6 +21,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Profile updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path, status: :see_other, notice: "User deleted successfully."
+  end
+
   private
 
   def user_params
