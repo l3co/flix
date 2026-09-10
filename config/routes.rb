5017175caa6d7722cbb88165b0632f1274049d26
l3co <lcosta.sp.br@gmail.com>
2026-09-10 10:27:55 -0300

    feat : implement user sign in functionality with session management

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   resources :movies do
     resources :reviews
   end
+  resource :session, only: [:new, :create, :destroy]
   resources :users
+  get "signin", to: "sessions#new"
   get "signup", to: "users#new"
 end
