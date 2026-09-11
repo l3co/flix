d4b1922bb0df114d72900dc4f8b3dde6e463ea00
l3co <lcosta.sp.br@gmail.com>
2026-09-11 20:46:42 -0300

    feat : add current_user helper and require_login before_action for user authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
 
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
+
+  helper_method :current_user
+
+  private
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def require_login
+    unless session[:user_id]
+      redirect_to signin_path, alert: "You must be logged in to access this section."
+    end
+  end
 end
